90c199ea836c51c7cea978f32b292bddf7e68bb0
DESKTOP-FP75HIB\User <sidi.meisanjaya2013@gmail.com>
2021-07-17 16:14:52 +0900

    akar akarnya tuan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $newUser = new User;
+        $newUser->id_user_type = 1;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,60 +29,57 @@ Route::get('/home/jobs', function () {
     return view('jobs');
 });
 
-Route::get('/home/job-details', function(){
+Route::get('/home/job-details', function () {
     return view('job-details');
 });
 
-Route::get('/home/about-us', function() {
+Route::get('/home/about-us', function () {
     return view('about-us');
 });
 
-Route::get('/home/team', function() {
+Route::get('/home/team', function () {
     return view('team');
 });
 
-Route::get('/home/blog', function() {
+Route::get('/home/blog', function () {
     return view('blog');
 });
 
-Route::get('/home/blog-details', function() {
+Route::get('/home/blog-details', function () {
     return view('blog-details');
 });
 
-Route::get('/home/terms', function() {
+Route::get('/home/terms', function () {
     return view('terms');
 });
 
-Route::get('home/contact', function() {
+Route::get('home/contact', function () {
     return view('contact');
 });
 
-Route::get('home/testimonials', function() {
+Route::get('home/testimonials', function () {
     return view('testimonials');
 });
 
-Route::get('home/seeker', function() {
+Route::get('home/seeker', function () {
     return view('seeker_dashboard');
 });
 
 
-Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/feedback', [App\Http\Controllers\FeedbackController::class, 'index'])->name('feedback');
+// Route::get('/recruit_dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('recruit_dashboard');
+// Route::get('/seeker_dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('seeker_dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/feedback', [App\Http\Controllers\FeedbackController::class, 'index'])->name('feedback');
-Route::get('/recruit_dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('recruit_dashboard');
-Route::get('/seeker_dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('seeker_dashboard');
 
-Auth::routes();
+// Route::get('/home', function () {
+//     return view('home');
+// })->name('home')->middleware('auth');
 
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
+// Route::get('/feedback', function () {
+//     return view('feedback');
+// })->name('feedback')->middleware('auth');
 
-Route::get('/feedback', function() {
-    return view('feedback');
-})->name('feedback')->middleware('auth');
-
-Route::get('/recruit_dashboard', function() {
-    return view('recruit_dashboard');
-})->name('recruit_dashboard')->middleware('auth');
+// Route::get('/recruit_dashboard', function () {
+//     return view('recruit_dashboard');
+// })->name('recruit_dashboard')->middleware('auth');
